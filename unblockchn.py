0d53d8dcae564fbe904a7a6414a3dd8ccd92a484
Steven Zhu <steven@stevenz.xyz>
2019-06-26 11:51:56 -0400

    Fix spacing

--- unblockchn.py
+++ unblockchn.py
@@ -1315,18 +1315,18 @@ Unblock CHN
         elogger.info("✔ 生成 ACL ruleset 文件（acl 目录）：pac.txt")
     @classmethod
     def is_ipv4(ip):
-    	match = re.match("^(\d{0,3})\.(\d{0,3})\.(\d{0,3})\.(\d{0,3})$", ip)
-    	if not match:
-    		return False
-    	quad = []
-    	for number in match.groups():
-    		quad.append(int(number))
-    	if quad[0] < 1:
-    		return False
-    	for number in quad:
-    		if number > 255 or number < 0:
-			return False
-	return True
+        match = re.match("^(\d{0,3})\.(\d{0,3})\.(\d{0,3})\.(\d{0,3})$", ip)
+        if not match:
+            return False
+        quad = []
+        for number in match.groups():
+            quad.append(int(number))
+        if quad[0] < 1:
+            return False
+        for number in quad:
+            if number > 255 or number < 0:
+            return False
+    return True
 
     @classmethod
     def cp_ruleset_file(cls, dst):
